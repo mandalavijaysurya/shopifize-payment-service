ceb077ad42d33354c62066287d90526ab3ead310
Lynx <mandala.vijay.surya@gmail.com>
2024-10-23 23:14:56 +0530

    feat: complete integration of stripe payment gateway

--- src/main/java/org/scaler/ecommercepaymentservice/controllers/WebhookController.java
+++ src/main/java/org/scaler/ecommercepaymentservice/controllers/WebhookController.java
@@ -1,7 +1,17 @@
-package org.scaler.ecommercepaymentservice.controllers;/**
+package org.scaler.ecommercepaymentservice.controllers;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
  * @author: Vijaysurya Mandala
  * @github: github/mandalavijaysurya (<a href="https://www.github.com/mandalavijaysurya"> Github</a>)
  */
+@RestController
 public class WebhookController {
-
+    @PostMapping("/webhook")
+    private void webhook(@RequestBody Object o){
+        System.out.println("DEBUG");
+    }
 }
--- src/main/java/org/scaler/ecommercepaymentservice/services/paymentgateways/StripePaymentGateway.java
+++ src/main/java/org/scaler/ecommercepaymentservice/services/paymentgateways/StripePaymentGateway.java
@@ -1,6 +1,13 @@
 package org.scaler.ecommercepaymentservice.services.paymentgateways;
 
+import com.stripe.Stripe;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentLink;
+import com.stripe.model.Price;
+import com.stripe.param.PaymentLinkCreateParams;
+import com.stripe.param.PriceCreateParams;
 import org.scaler.ecommercepaymentservice.dto.PaymentRequestDTO;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 /**
@@ -12,9 +19,54 @@ import org.springframework.stereotype.Component;
 @Component
 public class StripePaymentGateway implements PaymentGateway {
 
+    @Value("${stripe.key.id}")
+    private String stripeSecretKey;
+
     @Override
     public String generatePaymentLink(PaymentRequestDTO paymentRequestDTO) {
-        return "";
+        try{
+            Stripe.apiKey = this.stripeSecretKey;
+            Price price = getPrice(Long.parseLong(paymentRequestDTO.getAmount()));
+
+            PaymentLinkCreateParams params =
+                    PaymentLinkCreateParams.builder()
+                            .addLineItem(
+                                    PaymentLinkCreateParams.LineItem.builder()
+                                            .setPrice(price.getId())
+                                            .setQuantity(1L)
+                                            .build()
+                            ).setAfterCompletion(
+                                    PaymentLinkCreateParams.AfterCompletion.builder()
+                                    .setType(PaymentLinkCreateParams.AfterCompletion.Type.REDIRECT)
+                                    .setRedirect(
+                                            PaymentLinkCreateParams.AfterCompletion.Redirect.builder()
+                                            .setUrl("https://google.com?ref_id=" + "1234").build()
+                                    ).build()
+                            ).build();
+            PaymentLink paymentLink = PaymentLink.create(params);
+            return paymentLink.getUrl();
+        }catch (StripeException exception) {
+            throw new RuntimeException(exception.getMessage());
+        }
+    }
+
+    private Price getPrice(Long productAmount){
+        try{
+            PriceCreateParams params = PriceCreateParams.builder()
+                    .setCurrency("inr")
+                    .setUnitAmount(productAmount)
+                    .setProductData(
+                            PriceCreateParams
+                                    .ProductData
+                                    .builder()
+                                    .setName("PS5")
+                                    .build()
+                    )
+                    .build();
+            return Price.create(params);
+        }catch(StripeException ex){
+            throw new RuntimeException(ex.getMessage());
+        }
     }
 
 }
